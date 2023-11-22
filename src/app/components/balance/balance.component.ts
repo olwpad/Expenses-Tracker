98b3974ab256fcc1ed2b8ece5dd12d5dfedb1c86
olwpad <olwpad@gmail.com>
2023-11-21 21:09:02 -0500

    Se Agregó componente transactions y balance

---
+++ src/app/components/balance/balance.component.ts
@@ -0,0 +1,15 @@
+import { Component,Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Balance } from '../../models/balance.model';
+@Component({
+  selector: 'app-balance',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './balance.component.html',
+  styleUrl: './balance.component.scss'
+})
+export class BalanceComponent {
+  @Input() balance!: Balance;
+  };
+
